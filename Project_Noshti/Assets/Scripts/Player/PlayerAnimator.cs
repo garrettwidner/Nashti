30e634187f1949cee03ad916facc89bbefe202af
Garrett Widner <garrettrwidner@gmail.com>
2018-05-05 21:19:55 -0700

    Added final animation, the jump animation

--- Project_Noshti/Assets/Scripts/Player/PlayerAnimator.cs
+++ Project_Noshti/Assets/Scripts/Player/PlayerAnimator.cs
@@ -56,6 +56,8 @@ public class PlayerAnimator : MonoBehaviour
         {
             animator.SetBool("IsMoving", false);
         }
+
+        animator.SetBool("IsGrounded", platformingController.IsGrounded);
     }
 
 }

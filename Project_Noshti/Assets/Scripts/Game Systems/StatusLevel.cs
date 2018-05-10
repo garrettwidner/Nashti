a6304272a5f4ff1808415bcecefaed7e63d28d11
Garrett Widner <garrettrwidner@gmail.com>
2018-05-09 18:39:06 -0700

    More work on statusTracker

--- Project_Noshti/Assets/Scripts/Game Systems/StatusLevel.cs
+++ Project_Noshti/Assets/Scripts/Game Systems/StatusLevel.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class StatusLevel : MonoBehaviour
 {
-    [SerializeField] private int startingLevel;
-    [SerializeField] private int maxLevel;
+    [SerializeField] private int startingLevel = 100;
+    [SerializeField] private int maxLevel = 100;
     [Range(0, 50)]
     [Tooltip("Represents a percentage of the status bar per second")]
     [SerializeField] private float rapidIncrementSpeed;
@@ -13,70 +13,160 @@ public class StatusLevel : MonoBehaviour
     [Tooltip("Represents a percentage of the status bar per second")]
     [SerializeField] private float slowIncrementSpeed;
 
-    private float currentLevel;
-    private float toIncrementSlow;
-    private float toIncrementRapid;
+    private float minimumAllowedError = 0.01f;
+
+    private float statusLevel;
+    private float slowIncrementPool;
+    private float rapidIncrementPool;
+
+    private float slowGoal;
+    private float rapidGoal;
+
+    public float TEST_RAPID_INCREMENT = 30f;
+    public float TEST_SLOW_INCREMENT = 30f;
+    public float TEST_IMMEDIATE_INCREMENT = 30f;
+
+    public bool IsIncrementing
+    {
+        get
+        {
+            if(rapidIncrementPool != 0 || slowIncrementPool != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
 
     private void Start()
     {
-        currentLevel = startingLevel > maxLevel ? maxLevel : startingLevel;
+        statusLevel = startingLevel > maxLevel ? maxLevel : startingLevel;
     }
 
     public void RapidIncrement(float increment)
     {
-        toIncrementRapid += increment;
+        rapidIncrementPool += increment;
+        KeepStatusLevelWithinBounds();
+
     }
 
     public void SlowIncrement(float increment)
     {
-        toIncrementSlow += increment;
+        slowIncrementPool += increment;
+        KeepStatusLevelWithinBounds();
     }
 
     public void ImmediateIncrement(float increment)
     {
-        currentLevel += increment;
+        statusLevel += increment;
     }
 
     private void Update()
-    {
-        if(toIncrementSlow > 0)
+    { 
+        RunTest();
+
+        if(statusLevel != 0)
         {
-            RunSlowIncrement();
-        }
+            if (slowIncrementPool != 0)
+            {
+                RunIncrement(ref slowIncrementPool, slowIncrementSpeed);
+            }
 
-        if(toIncrementRapid > 0)
-        {
-            RunRapidIncrement();
-        }
+            if (rapidIncrementPool != 0)
+            {
+                RunIncrement(ref rapidIncrementPool, rapidIncrementSpeed);
+            }
 
-        KeepCurrentLevelWithinBounds();
-    }
-
-    private void RunRapidIncrement()
-    {
-        float currentIncrement = rapidIncrementSpeed * Time.deltaTime;
-        currentLevel -= currentIncrement;
-    }
-
-    private void RunSlowIncrement()
-    {
-        float currentIncrement = slowIncrementSpeed * Time.deltaTime;
-        currentLevel -= currentIncrement;
-    }
-
-    private void KeepCurrentLevelWithinBounds()
-    {
-        if(currentLevel < 0)
-        {
-            currentLevel = 0;
-        }
-        if(currentLevel > maxLevel)
-        {
-            currentLevel = maxLevel;
+            KeepStatusLevelWithinBounds();
+            StabilizeIncrement(ref rapidIncrementPool);
+            StabilizeIncrement(ref slowIncrementPool);
         }
     }
 
+    private void RunTest()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            RapidIncrement(TEST_RAPID_INCREMENT);
+            print("---------- Set to increment " + TEST_RAPID_INCREMENT + " rapidly.");
+        }
+        else if (Input.GetKeyDown(KeyCode.K))
+        {
+            SlowIncrement(TEST_SLOW_INCREMENT);
+            print("---------- Set to increment " + TEST_SLOW_INCREMENT + " slowly.");
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            ImmediateIncrement(TEST_IMMEDIATE_INCREMENT);
+            print("---------- Set to increment " + TEST_IMMEDIATE_INCREMENT + " rapidly.");
+            print(statusLevel);
+        }
+    }
 
-    
+    private void RunIncrement(ref float incrementPool, float incrementSpeed)
+    {
+        bool incrementIsEnding = false;
+        float framewiseIncrement = incrementSpeed * Time.deltaTime * Mathf.Sign(incrementPool);
+        if (Mathf.Abs(framewiseIncrement) > Mathf.Abs(incrementPool))
+        {
+            framewiseIncrement = incrementPool;
+            incrementIsEnding = true;
+        }
+        incrementPool -= framewiseIncrement;
+        statusLevel += framewiseIncrement;
 
+        if(incrementIsEnding)
+        {
+            KeepStatusLevelClean();
+        }
+
+        print(statusLevel);
+    }
+
+    private void KeepStatusLevelClean()
+    {
+        //TODO:-----------------------Check for opposite way, say if it settles on 39.9998
+        int statusInt = (int)statusLevel;
+        float statusDecimal = statusLevel - statusInt;
+
+        if(statusDecimal < minimumAllowedError)
+        {
+            statusLevel = statusInt;
+        }
+    }
+
+    private void KeepStatusLevelWithinBounds()
+    {
+        if(statusLevel <= 0)
+        {
+            statusLevel = 0;
+            print("Status level below zero. Incrementing stopped");
+            ResetIncrements();
+        }
+        if(statusLevel > maxLevel)
+        {
+            statusLevel = maxLevel;
+            print("Status level above maximum. Incrementing stopped");
+            //TODO: What if we want a timed heal effect that will last through damage done?
+            //like healing on a timer that heals a certain amount every frame no matter what else changes
+            ResetIncrements();
+        }
+    }
+
+    private void StabilizeIncrement(ref float increment)
+    {
+        if(Mathf.Abs(increment) <= minimumAllowedError)
+        {
+            increment = 0;
+        }
+    }
+
+    private void ResetIncrements()
+    {
+        //TODO------------------: Only reset increments if they're not going in opposite directions.
+        //If we reach the top with speedy healing and we're in poison we don't want to stop the poison.
+        //Brings up: How do we deal with constant drains like poison? We need to be able to start and stop it.
+        rapidIncrementPool = 0;
+        slowIncrementPool = 0;
+    }
 }
